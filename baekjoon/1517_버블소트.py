6fb644ed41bd33cc160964e1e67f62bcaa778259
Yeji Moon <yeaji21@gmail.com>
2020-10-23 07:29:54 +0900

    Fri, Oct 23, 2020  7:29:54 AM

--- baekjoon/1517_버블소트.py
+++ baekjoon/1517_버블소트.py
@@ -5,10 +5,9 @@ n = int(input())
 arr = list(map(int, input().split()))
 result = 0
 
-for i in range(0, n - 1):
-    for j in range(i + 1, n):
+for i in range(0, n):
+    for j in range(i, n):
         if arr[i] > arr[j]:
-            arr[i], arr[j] = arr[j], arr[i]
             result += 1
 
 print(result)
--- baekjoon/15237_Cipher.py
+++ baekjoon/15237_Cipher.py
@@ -1,8 +1,18 @@
 import sys
+from collections import OrderedDict
 input = sys.stdin.readline
 
 n, c = map(int, input().split())
-arr = list(map(int, input().split()))
+messages = list(input().split())
+ordered_dict = OrderedDict()
 
+for message in messages:
+    if message in ordered_dict:
+        ordered_dict[message] += 1
+    else:
+        ordered_dict[message] = 1
 
+sorted_dict = {k: v for k, v in sorted(ordered_dict.items(), key=lambda item: item[1], reverse=True)}
 
+for k, v in sorted_dict.items():
+    print(' '.join([k] * v), end=' ')
