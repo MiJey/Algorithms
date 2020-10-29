3d6ca3b1c52e154b956e567d729e36e907e11e39
Yeji Moon <yeaji21@gmail.com>
2020-10-30 06:23:32 +0900

    Fri, Oct 30, 2020  6:23:32 AM

--- baekjoon/3190_뱀.py
+++ baekjoon/3190_뱀.py
@@ -2,36 +2,64 @@ import sys
 
 input = sys.stdin.readline
 
-n = int(input())
-
-# 0~3: 뱀(방향), -1: 아무것도 없음, -2: 사과
-board = [[-1] * n for _ in range(n)]
-board[0][0] = 0
-
-# → ↑ ← ↓
+# 뱀 방향 → ↑ ← ↓
 directions = [[0, -1, 0, 1], [1, 0, -1, 0]]
 cur = 0
+
+# 뱀 머리, 꼬리 좌표
 head = [0, 0]
 tail = [0, 0]
 
-t = 0
-count = 0
+# 0~3: 뱀 방향, -1: 아무것도 없음, -2: 사과
+n = int(input())
+board = [[-1] * n for _ in range(n)]
+board[0][0] = 0
 
+# 시간(초)
+time = 0
+
+
+def print_board():
+    print("PLAY TIME:", time)
+
+    for i in range(n):
+        for j in range(n):
+            if board[i][j] == -1:
+                print("□", end='')
+            elif board[i][j] == -2:
+                print("♥", end='')
+            elif board[i][j] == 0:
+                print("▶", end='')
+            elif board[i][j] == 1:
+                print("▲", end='')
+            elif board[i][j] == 2:
+                print("◀", end='')
+            elif board[i][j] == 3:
+                print("▼", end='')
+        print()
+
+    for _ in range(n):
+        print("―", end='')
+
+    print()
+
+
+# 사과 위치
 for _ in range(int(input())):
     a, b = map(int, input().split())
     board[a - 1][b - 1] = -2
 
-# for i in range(n):
-#     print(board[i])
+# print_board()
 
+# x초에 방향 회전 정보
 change_direction = dict()
+
 for _ in range(int(input())):
     x, c = input().split()
     change_direction[int(x)] = c
 
 while True:
-    count += 1
-    # print("count", count)
+    time += 1
 
     # 다음 칸 좌표
     head = [head[0] + directions[0][cur], head[1] + directions[1][cur]]
@@ -39,7 +67,7 @@ while True:
     # 벽에 부딪힌 경우, 자기 몸에 부딪힌 경우
     if head[0] < 0 or head[0] >= n or head[1] < 0 or head[1] >= n \
             or board[head[0]][head[1]] >= 0:
-        print(count)
+        print(time)
         exit()
 
     next_cell = board[head[0]][head[1]]
@@ -54,8 +82,9 @@ while True:
         tail[0] += directions[0][next_tail_dir]
         tail[1] += directions[1][next_tail_dir]
 
-    if count in change_direction:
-        if change_direction[count] == "L":
+    # x초에 방향 회전
+    if time in change_direction:
+        if change_direction[time] == "L":
             # 왼쪽 회전
             cur = (cur + 1) % 4
         else:
@@ -66,6 +95,4 @@ while True:
 
     board[head[0]][head[1]] = cur
 
-    # for i in range(n):
-    #     print(board[i])
-    # print("----------")
+    # print_board()
