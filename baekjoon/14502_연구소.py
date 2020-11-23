ce7935f7b446559ed38a17e9f93cc16daaccfeab
Yeji Moon <yeaji21@gmail.com>
2020-11-23 19:19:57 +0900

    Mon Nov 23 19:19:57 KST 2020

--- baekjoon/14502_연구소.py
+++ baekjoon/14502_연구소.py
@@ -1,3 +1,4 @@
+# 예제만 통과
 import sys
 input = sys.stdin.readline
 
@@ -19,22 +20,19 @@ for i in range(0, n):
 
 
 def get_virus_area_size():
-    # 바이러스가 퍼진 영역의 크기 반환
     virus_area = [[0] * m for _ in range(n)]
+    virus_area_size = 0
 
     def spread_virus(i, j):
         # 벽이면 리턴
         if i < 0 or j < 0 or i >= n or j >= m or lab[i][j] == 1:
-            # print("벽이라 리턴", i, j)
             return 0
 
         # 이미 바이러스가 퍼진 곳이면 리턴
         if virus_area[i][j] == 2:
-            # print("이미 바이러스가 퍼져서 리턴", i, j)
             return 0
 
         # 바이러스 퍼짐
-        # print("바이러스 퍼짐", i, j)
         virus_area[i][j] = 2
 
         return 1 \
@@ -43,38 +41,35 @@ def get_virus_area_size():
             + spread_virus(i, j - 1) \
             + spread_virus(i, j + 1)
 
-
-    virus_area_size = 0
-
     for x in range(0, n):
         for y in range(0, m):
             # 바이러스 시작점부터 퍼트리기
             if lab[x][y] == 2:
-                # print("바이러스 시작점부터 퍼트리기", x, y)
                 virus_area_size += spread_virus(x, y)
 
+    # 바이러스가 퍼진 영역의 크기 반환
     return virus_area_size
 
 
 # 벽 3개 브루트포스로 고르기
-for x in range(0, m * n - 2):
+for x in range(0, m * n - 3):
     # x = i * m + j
-    xi = x // n
-    xj = x % n
+    xi = x // m
+    xj = x % m
 
     if lab[xi][xj] != 0:
         continue
 
-    for y in range(x + 1, m * n - 1):
-        yi = y // n
-        yj = y % n
+    for y in range(x + 1, m * n - 2):
+        yi = y // m
+        yj = y % m
 
         if lab[yi][yj] != 0:
             continue
 
-        for z in range(y + 1, m * n):
-            zi = z // n
-            zj = z % n
+        for z in range(y + 1, m * n - 1):
+            zi = z // m
+            zj = z % m
 
             if lab[zi][zj] != 0:
                 continue
@@ -84,23 +79,13 @@ for x in range(0, m * n - 2):
             lab[yi][yj] = 1
             lab[zi][zj] = 1
 
-            print("====================")
-            print(x, y, z)
-            for asdf in range(0, n):
-                print(lab[asdf])
-
             # 안전 영역의 크기 구하기
             safety_area_size = lab_size - wall_area_size - get_virus_area_size()
-            print("lab_size", lab_size)
-            print("wall_area_size", wall_area_size)
-            print("safety_area_size", safety_area_size)
-
             result = max(result, safety_area_size)
 
             # 벽 3개 리셋
             lab[xi][xj] = 0
             lab[yi][yj] = 0
             lab[zi][zj] = 0
-
 
 print(result)
