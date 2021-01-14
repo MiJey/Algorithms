4ca66927c767196e032ba9b1a3b19c9a3d30dd47
Yeji Moon <yeaji21@gmail.com>
2021-01-14 20:09:58 +0900

    2021년 01월 14일 목 오후  8:09:58

--- baekjoon/11053_가장긴증가하는부분수열.py
+++ baekjoon/11053_가장긴증가하는부분수열.py
@@ -5,14 +5,14 @@ input = sys.stdin.readline
 N = int(input())
 arr = list(map(int, input().split()))
 dp = [1] * N
-result = 0
+result = 1
 
 for i in range(1, N):
     # 내가 꼬리로 붙을 수 있는 부분 수열 찾기 -> 그 중 가장 긴걸 선택
     for j in range(i):
         # 이전 꼬리가 나보다 숫자가 작아야 함
-        # 이전 꼬리의 길이가 제일 긴 것으로 선택
-        if arr[j] < arr[i] and dp[j] + 1 > dp[i]:
+        # 이전 꼬리의 길이가 같거나(+1 하니까) 길어야 더 긴 꼬리!
+        if arr[j] < arr[i] and dp[j] >= dp[i]:
             dp[i] = dp[j] + 1
 
     if dp[i] > result:
